d113b0ecddfc01f178296a789ddae479218d1c60
Michael Mullin <masmullin@gmail.com>
2022-06-03 11:58:32 -0400

    change to benching numbers vs letters

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,45 +1,143 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use ref_simple::{add_them, add_them2, add_them3};
+use ref_simple::{
+    add_them, add_them2, add_them3, make_random_string, numbers_letters, numbers_letters_fold,
+    numbers_letters_iter, numbers_letters_orig, numbers_letters_part,
+};
 
-fn run_test(name: &str, time: u8, c: &mut Criterion) {
+const TIME: u64 = 12;
+
+fn run_test(name: &str, str_size: usize, c: &mut Criterion) {
+    //let mumble = "a1b2c3d4e5f6"; //make_random_string(255);
+    let all_punc = make_random_string(str_size, 0, 0);
+    let some_punc = make_random_string(str_size, 66, 0);
+    let all_alpha = make_random_string(str_size, 100, 0);
+    let all_num = make_random_string(str_size, 0, 100);
+    let halvies = make_random_string(str_size, 50, 50);
+    let fairs = make_random_string(str_size, 34, 33);
+    macro_rules! make_bench {
+        ($g:ident, $name:ident, $func:ident, $data:ident) => {
+            $g.bench_function($name, |b| {
+                b.iter(|| {
+                    black_box($func(&$data));
+                })
+            });
+        };
+    }
+
+    macro_rules! make_benches {
+        ($g:ident, $name:literal, $func:ident) => {
+            let name = $name.to_string();
+
+            let all_punc_name = name.clone() + "_all_punc";
+            make_bench!($g, all_punc_name, $func, all_punc);
+
+            let some_punc_name = name.clone() + "_some_punc";
+            make_bench!($g, some_punc_name, $func, some_punc);
+
+            let all_alpha_name = name.clone() + "_all_alpha";
+            make_bench!($g, all_alpha_name, $func, all_alpha);
+
+            let all_num_name = name.clone() + "_all_num";
+            make_bench!($g, all_num_name, $func, all_num);
+
+            let halvies_name = name.clone() + "_halvies";
+            make_bench!($g, halvies_name, $func, halvies);
+
+            let fairs_name = name.clone() + "_fairs";
+            make_bench!($g, fairs_name, $func, fairs);
+        };
+    }
+
     let mut g = c.benchmark_group(name);
-    g.measurement_time(std::time::Duration::from_secs(time as u64));
-    g.bench_function("add_them", |b| {
-        b.iter(|| {
-            let x = 1;
-            let y = 2;
-            black_box(add_them(&x, &y))
-        })
-    });
-    g.bench_function("add_them2", |b| {
-        b.iter(|| {
-            let x = 1;
-            let y = 2;
-            black_box(add_them2(x, y));
-        })
-    });
-    g.bench_function("add_them3", |b| {
-        b.iter(|| {
-            let x = 1;
-            let y = 2;
-            black_box(add_them3(x, &y));
-        })
-    });
-    //g.bench_function("unstable sort", |b| b.iter(|| black_box(ans_unst.sort_unstable())));
+    //g.measurement_time(std::time::Duration::from_secs(TIME));
+    make_benches!(g, "opt", numbers_letters);
+    make_benches!(g, "orig", numbers_letters_orig);
+    make_benches!(g, "iter", numbers_letters_iter);
+    make_benches!(g, "part", numbers_letters_part);
+    make_benches!(g, "fold", numbers_letters_fold);
+    /*g.bench_function("num_let_orig", |b| {*/
+    /*b.iter(|| {*/
+    /*black_box(numbers_letters_orig(&no_punc));*/
+    /*})*/
+    /*});*/
+    /*g.bench_function("num_let_iter", |b| {*/
+    /*b.iter(|| {*/
+    /*black_box(numbers_letters_iter(&no_punc));*/
+    /*})*/
+    /*});*/
+    /*g.bench_function("num_let_part", |b| {*/
+    /*b.iter(|| {*/
+    /*black_box(numbers_letters_part(&no_punc));*/
+    /*})*/
+    /*});*/
+    /*g.bench_function("num_let_fold", |b| {*/
+    /*b.iter(|| {*/
+    /*black_box(numbers_letters_fold(&no_punc));*/
+    /*})*/
+    /*});*/
+
+    /*g.bench_function("num_let_punc", |b| {*/
+    /*b.iter(|| {*/
+    /*black_box(numbers_letters(&with_punc));*/
+    /*})*/
+    /*});*/
+    /*g.bench_function("num_let_orig_punc", |b| {*/
+    /*b.iter(|| {*/
+    /*black_box(numbers_letters_orig(&with_punc));*/
+    /*})*/
+    /*});*/
+    /*g.bench_function("num_let_iter_punc", |b| {*/
+    /*b.iter(|| {*/
+    /*black_box(numbers_letters_iter(&with_punc));*/
+    /*})*/
+    /*});*/
+    /*g.bench_function("num_let_part_punc", |b| {*/
+    /*b.iter(|| {*/
+    /*black_box(numbers_letters_part(&with_punc));*/
+    /*})*/
+    /*});*/
     g.finish();
 }
 
 fn bench_tiny(c: &mut Criterion) {
-    run_test("tiny", 10, c);
+    run_test("tiny", 8, c);
 }
 
 fn bench_medium(c: &mut Criterion) {
-    run_test("medium", 30, c);
+    run_test("medium", 255, c);
 }
 
 fn bench_long(c: &mut Criterion) {
-    run_test("long", 50, c);
+    run_test("long", 4096, c);
 }
 
-criterion_group!(benches, bench_tiny, bench_medium, bench_long);
+fn bench_signal(c: &mut Criterion) {
+    let signal7 = match std::fs::read_to_string("signal.7.html") {
+        Ok(s) => s,
+        _ => "signal.7.html".to_string(),
+    };
+
+    macro_rules! make_bench {
+        ($g:ident, $name:literal, $func:ident, $data:ident) => {
+            $g.bench_function($name, |b| {
+                b.iter(|| {
+                    black_box($func(&$data));
+                })
+            });
+        };
+    }
+
+    let mut g = c.benchmark_group("signal");
+    g.measurement_time(std::time::Duration::from_secs(TIME));
+
+    make_bench!(g, "opt_signal", numbers_letters, signal7);
+    make_bench!(g, "orig_signal", numbers_letters_orig, signal7);
+    make_bench!(g, "iter_signal", numbers_letters_iter, signal7);
+    make_bench!(g, "part_signal", numbers_letters_part, signal7);
+    make_bench!(g, "fold_signal", numbers_letters_fold, signal7);
+
+    g.finish();
+}
+
+criterion_group!(benches, bench_signal, bench_tiny, bench_medium, bench_long);
 criterion_main!(benches);
--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,142 @@ pub fn add_them2(x: u32, y: u32) -> u64 {
 pub fn add_them3(x: u32, y: &u32) -> u64 {
     (x as u64) + (*y as u64)
 }
+
+pub fn make_random_string(len: usize, mut letters: u8, mut numbers: u8) -> String {
+    use rand::Rng;
+
+    const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+                             abcdefghijklmnopqrstuvwxyz";
+    const NUMSET: &[u8] = b"0123456789";
+    const PUNCSET: &[u8] = b"!@#$%^&*()_+=,.<>[]{}\\";
+
+    if letters > 100 {
+        letters = 100;
+        numbers = 0;
+    } else {
+        numbers = letters + numbers;
+        if numbers > 100 {
+            numbers = 100;
+        }
+    }
+
+    let punc = 100 - numbers;
+    assert_eq!(100, numbers + punc);
+
+    let mut rng = rand::thread_rng();
+
+    let password: String = (0..len)
+        .map(|_| {
+            let chance = rng.gen_range(1..100);
+            if chance <= letters {
+                let idx = rng.gen_range(0..CHARSET.len());
+                CHARSET[idx] as char
+            } else if chance <= numbers {
+                let idx = rng.gen_range(0..NUMSET.len());
+                NUMSET[idx] as char
+            } else {
+                let idx = rng.gen_range(0..PUNCSET.len());
+                PUNCSET[idx] as char
+            }
+        })
+        .collect();
+
+    password
+}
+
+pub fn numbers_letters(orig: &str) -> (String, String) {
+    let guess_len = orig.len();
+    let mut letters = String::with_capacity(guess_len);
+    let mut numbers = String::with_capacity(guess_len);
+
+    for c in orig.chars() {
+        if !c.is_alphanumeric() {
+            continue;
+        } else if c.is_alphabetic() {
+            letters.push(c);
+        } else if c.is_digit(10) {
+            numbers.push(c);
+        }
+    }
+    letters.shrink_to_fit();
+    numbers.shrink_to_fit();
+
+    (letters, numbers)
+}
+
+pub fn numbers_letters_orig(orig: &str) -> (String, String) {
+    let mut letters = String::new();
+    let mut numbers = String::new();
+
+    for c in orig.chars() {
+        if c.is_digit(10) {
+            numbers.push(c);
+        }
+        if c.is_alphabetic() {
+            letters.push(c);
+        }
+    }
+
+    (letters, numbers)
+}
+
+pub fn numbers_letters_iter(orig: &str) -> (String, String) {
+    let numbers: String = orig.chars().filter(|c| c.is_digit(10)).collect();
+    let letters: String = orig.chars().filter(|c| c.is_alphabetic()).collect();
+
+    (letters, numbers)
+}
+
+pub fn numbers_letters_part(orig: &str) -> (String, String) {
+    let (letters, numbers): (String, String) = orig
+        .chars()
+        .filter(|x| x.is_alphanumeric())
+        .partition(|c| c.is_alphabetic());
+
+    (letters, numbers)
+}
+
+pub fn numbers_letters_fold(orig: &str) -> (String, String) {
+    let guess_len = orig.len();
+    let (mut letters, mut numbers) = orig.chars().fold(
+        (
+            String::with_capacity(guess_len),
+            String::with_capacity(guess_len),
+        ),
+        |(mut letters, mut numbers), c| {
+            if c.is_digit(10) {
+                numbers.push(c);
+            } else if c.is_alphabetic() {
+                letters.push(c);
+            }
+            (letters, numbers)
+        },
+    );
+
+    letters.shrink_to_fit();
+    numbers.shrink_to_fit();
+
+    (letters, numbers)
+}
+
+#[test]
+fn test_functions() {
+    for _ in 0..100 {
+        let mumble = make_random_string(1024, 66, 33);
+        let (orig_str, orig_num) = numbers_letters_orig(&mumble);
+        let (opt_str, opt_num) = numbers_letters(&mumble);
+        let (iter_str, iter_num) = numbers_letters_iter(&mumble);
+        let (part_str, part_num) = numbers_letters_part(&mumble);
+        let (fold_str, fold_num) = numbers_letters_fold(&mumble);
+
+        assert_eq!(orig_num, opt_num);
+        assert_eq!(orig_num, iter_num);
+        assert_eq!(orig_num, part_num);
+        assert_eq!(orig_num, fold_num);
+
+        assert_eq!(orig_str, opt_str);
+        assert_eq!(orig_str, iter_str);
+        assert_eq!(orig_str, part_str);
+        assert_eq!(orig_str, fold_str);
+    }
+}
